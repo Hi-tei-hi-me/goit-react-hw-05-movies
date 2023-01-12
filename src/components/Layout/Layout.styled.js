ca9aecad7e1412f968b66823fc32199244ddb0f9
Hi-tei-hi-me <lethargium@gmail.com>
2023-01-12 21:53:24 +0200

    Header styled

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -1,8 +1,40 @@
 import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding-right: 15px;
+  padding-left: 15px;
+`;
 
-export const Header = styled.header``;
+export const Header = styled.header`
+  margin-right: -15px;
+  margin-left: -15px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  border-bottom: 2px solid gray;
+  background-color: #2c3e50;
+  color: white;
+  & nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    text-transform: uppercase;
+  }
+`;
 
-export const Link = styled(NavLink)``;
+export const Link = styled(NavLink)`
+  font-weight: bold;
+  padding: 10px;
+  border-radius: 3px;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  &:hover,
+  &:focus {
+    text-decoration: underline;
+  }
+  &.active {
+    background-color: #9c0309;
+    box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14),
+      0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+  }
+`;
