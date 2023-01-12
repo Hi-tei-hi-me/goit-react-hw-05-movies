f610db52e1527b8936072e19b91a6ec80be28aff
Hi-tei-hi-me <lethargium@gmail.com>
2023-01-12 21:04:55 +0200

    Loader UPD

--- src/utils/Loader/Loader.jsx
+++ src/utils/Loader/Loader.jsx
@@ -3,10 +3,10 @@ import { Watch } from 'react-loader-spinner';
 export const Loader = () => {
   return (
     <Watch
-      height="80"
-      width="80"
+      height="150"
+      width="150"
       radius="48"
-      color="#BE9750"
+      color="slategray"
       ariaLabel="watch-loading"
       wrapperStyle={{
         position: 'absolute',
