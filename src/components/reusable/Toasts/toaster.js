e1fa99fa54ae36d209a8d197f7a04f2da22125d0
Hi-tei-hi-me <lethargium@gmail.com>
2023-01-14 22:51:18 +0200

    Toaster styles UPD

--- src/components/reusable/Toasts/toaster.js
+++ src/components/reusable/Toasts/toaster.js
@@ -14,49 +14,49 @@ const toastConfig = {
     icon: <GiSkullInJar size={40} color="#9c0a21" />,
     style: {
       color: 'black',
-      background: 'rgba(218, 212, 212, 0.7)',
+      background: 'rgba(218, 212, 212, 0.8)',
     },
   },
   repeatedQuery: {
     icon: <GiSkullCrossedBones size={40} color="#9c0a21" />,
     style: {
       color: 'black',
-      background: 'rgba(218, 212, 212, 0.7)',
+      background: 'rgba(218, 212, 212, 0.8)',
     },
   },
   filmsFound: {
     icon: <GiHappySkull size={40} color="#9c0a21" />,
     style: {
       color: 'black',
-      background: 'rgba(218, 212, 212, 0.7)',
+      background: 'rgba(218, 212, 212, 0.8)',
     },
   },
   nothingFound: {
     icon: <GiChewedSkull size={40} color="#9c0a21" />,
     style: {
       color: 'black',
-      background: 'rgba(218, 212, 212, 0.7)',
+      background: 'rgba(218, 212, 212, 0.8)',
     },
   },
   noCastInfo: {
     icon: <GiSurprisedSkull size={40} color="#9c0a21" />,
     style: {
       color: 'black',
-      background: 'rgba(218, 212, 212, 0.7)',
+      background: 'rgba(218, 212, 212, 0.8)',
     },
   },
   reviewsFound: {
     icon: <GiGooSkull size={40} color="#9c0a21" />,
     style: {
       color: 'black',
-      background: 'rgba(218, 212, 212, 0.7)',
+      background: 'rgba(218, 212, 212, 0.8)',
     },
   },
   noReviews: {
     icon: <GiPirateSkull size={40} color="#9c0a21" />,
     style: {
       color: 'black',
-      background: 'rgba(218, 212, 212, 0.7)',
+      background: 'rgba(218, 212, 212, 0.8)',
     },
   },
 };
